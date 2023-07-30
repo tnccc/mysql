bd0b9381f74be0322b825294886695c02e05bc4d
tnccc <irei.frontend@gmail.com>
2023-07-30 21:13:45 +0900

    修正

--- sql2/01_script.sql
+++ sql2/01_script.sql
@@ -9,13 +9,16 @@ CREATE TABLE person_data(
 	address CHAR(30)
 );
 
-INSERT INTO person_data(id, name, age, address) 
+ALTER TABLE person_data ADD dept CHAR(3);
+
+
+INSERT INTO person_data(id, name, age, address, dept) 
 VALUES (1, "佐藤", 25, "東京都千代田区"),
 	(2, "鈴木", 32, "東京都北区"),
 	(3, "斎藤", 43, "神奈川県川崎市"),
 	(4, "吉田", 35, "神奈川県横浜市"),
 	(5, "川崎", 27, "東京都武蔵野市"),
 	(6, "石川", 53, "東京都北区");
-	
-SELECT * FROM person_data;
 
+SELECT * FROM person_data;
+SELECT dept, count(name) FROM person_data GROUP BY dept;
